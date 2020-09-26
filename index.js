333cf6f75a250df0ec7351858470138a969df9f1
uchihaNayeem <uchiha.nayeem@gmail.com>
2020-09-26 14:31:15 +0600

    My first Node & gitignore

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('I know how to open port')
+  res.send('I know how to open port & gitignore (node_modules)')
 })
 
 app.listen(4200, ()=>console.log("Lintening from port 4200"));
