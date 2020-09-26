8fe2457e460ec9b30cd4822c399cd5a8497e3614
uchihaNayeem <uchiha.nayeem@gmail.com>
2020-09-26 17:00:04 +0600

    getting URL id using params

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+// following lines are kindof default i guess
 const express = require('express');
 const app = express();
 
+// 01
 app.get('/', (req, res) => {
 
  const fruit={
@@ -14,6 +16,7 @@ app.get('/', (req, res) => {
 })
 
 
+//02
 app.get('/shop/banana', (req, res)=>{
 
   const realBanana = {
@@ -23,5 +26,22 @@ app.get('/shop/banana', (req, res)=>{
   res.send(realBanana)
 })
 
+//04
 
-app.listen(4200, ()=>console.log("Lintening from port 4200"));
+const person = ['rahim', 'karim', 'das papa', 'ryan dhal']
+
+app.get('/user/:id', (req, res) => {
+
+  const userId = req.params.id;
+  const name = person[userId] // akhane 'userId' index number er moto kaj korse
+
+  res.send({name, userId}) // json banai felse {} diye
+  
+  console.log(req.params.id) // URL er id dhortese
+})
+
+
+
+
+// opening port 
+app.listen(4200, () => console.log("Lintening from port 4200"));
