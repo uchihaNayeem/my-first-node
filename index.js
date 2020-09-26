421456a9f67e57b8b0fe89b9cef0ab947fb76d4a
uchihaNayeem <uchiha.nayeem@gmail.com>
2020-09-26 16:23:10 +0600

    nodemon installed n tested

--- index.js
+++ index.js
@@ -3,14 +3,25 @@ const app = express();
 
 app.get('/', (req, res) => {
 
-  const fruit={
+ const fruit={
     product: 'ada',
-    price: 200
+    price: 200,
+    quantiry: 100
   }
 
-
-
   res.send(fruit)
+
 })
+
+
+app.get('/shop/banana', (req, res)=>{
+
+  const realBanana = {
+    product: 'banana',
+    price: 200
+  }
+  res.send(realBanana)
+})
+
 
 app.listen(4200, ()=>console.log("Lintening from port 4200"));
