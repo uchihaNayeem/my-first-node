93ce00dcb8754b2a38f5b89e8f04fde6000a879b
uchihaNayeem <uchiha.nayeem@gmail.com>
2020-09-26 15:59:50 +0600

    nodemon installed n tested

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('I know how to open port & gitignore (node_modules)')
+
+  const fruit={
+    product: 'ada',
+    price: 200
+  }
+
+
+
+  res.send(fruit)
 })
 
 app.listen(4200, ()=>console.log("Lintening from port 4200"));
